87924b626af0228072556f578cbd98278493ecaf
Sam Clark <sam.clark1142@gmail.com>
2020-06-07 15:39:10 -0400

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 sudo modprobe w1-gpio
-sudo modprove w1-therm
-flask run app.py --host=0.0.0.0 --port=1142
+sudo modprobe w1-therm
+
+export FLASK_APP=GoatConditioner.py
+flask run --host=0.0.0.0 --port=1142
